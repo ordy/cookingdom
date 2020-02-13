d2b266d1a7c7ca32381015a111e32e05b441391e
Sandro T <ordysan@gmail.com>
2020-02-13 15:51:35 +0100

    created inventory and recipe components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventoryComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule
