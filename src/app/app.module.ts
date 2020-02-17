5da1d1dd89e6e375d6dc1c86ad8087118330a059
Sandro T <ordysan@gmail.com>
2020-02-18 00:49:55 +0100

    added AppRouting, created Ingredients component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { InventoryComponent } from './components/inventory/inventory.component';
 import { RecipeComponent } from './components/recipe/recipe.component';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { IngredientsComponent } from './components/ingredients/ingredients.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InventoryComponent,
-    RecipeComponent
+    RecipeComponent,
+    IngredientsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    NgbModule
+    NgbModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
