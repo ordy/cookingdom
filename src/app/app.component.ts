6f11bc1a3f97183fb13bcb669d19c633e1824270
Sandro T <ordysan@gmail.com>
2020-02-17 17:24:47 +0100

    search as you type ingredients

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+
+const ingredients = [
+  'Salt',
+  'Papper',
+  'Cinnamon',
+  'Ginger',
+  'Cumin',
+  'Curry',
+  'Tomato',
+  'Cucumber',
+  'Onion',
+  'Cabbage',
+  'Potato',
+  'Bell Pepper',
+  'Apple',
+  'Orange',
+  'Strawberry',
+  'Cherry',
+  'Pineapple',
+  'Lemon',
+  'Banana',
+  'Chicken',
+  'Lamb',
+  'Beef',
+  'Pork',
+  'Duck',
+  'Turkey',
+  'Crab',
+  'Lobster',
+  'Mussels',
+  'Clams',
+  'Oysters',
+  'Shrimps',
+  'Salmon',
+  'Tuna',
+  'Cod',
+  'Mackrel',
+  'Herring',
+  'Sardines',
+  'Fettuccine',
+  'Spaghetti',
+  'Penne',
+  'Fusilli',
+  'Farfalle',
+  'Macaroni'
+];
 
 @Component({
   selector: 'app-root',
@@ -7,6 +55,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'CooKingdom';
+  public selectedIngr: string;
+  public isMenuCollapsed = true;
+
+  search = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map(term => term === '' ? []
+        : ingredients.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) === 0).slice(0, 10))
+    )
 
   constructor() {}
   ngOnInit() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { InventoryComponent } from './components/inventory/inventory.component';
 import { RecipeComponent } from './components/recipe/recipe.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { RecipeComponent } from './components/recipe/recipe.component';
     RecipeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
