02154bb6af49b8dc1c11bcb2892e5a1109c850d3
Sandro T <ordysan@gmail.com>
2020-04-06 20:35:50 +0200

    recipe list

--- src/app/services/inventory.service.ts
+++ src/app/services/inventory.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
 
-interface Drop { type: number; value: number; name: string; }
+interface Drop { type: number; name: string; quantity: number }
 
 @Injectable({
   providedIn: 'root',
 })
 export class InventoryService {
-  myInventory: Drop[] = [];
+  myInventory: Drop[] = [
+    { type: 1, name: 'Banana', quantity: 2000},
+    { type: 2, name: 'Sardines', quantity: 10002},
+    { type: 1, name: 'Crab', quantity: 12221},
+    { type: 2, name: 'Salt', quantity: 22221},
+    { type: 2, name: 'Pepper', quantity: 999992},
+    { type: 2, name: 'Curry', quantity: 42222},
+    { type: 2, name: 'Macaroni', quantity: 222223},
+    { type: 2, name: 'Chicken', quantity: 32222}];
 
   constructor() {}
 
   addInventory(ingrList: Drop[]){
     for(const ingr of ingrList) {
-      const result = this.myInventory.findIndex(({ name }) => name === ingr.name);
-      if (result !== -1) {
+      if (this.ingrExists(ingr.name)) {
         this.updateInventory(ingr);
       } else {
         this.myInventory.push(ingr);
@@ -21,13 +28,24 @@ export class InventoryService {
     }
     console.log(this.myInventory);
   }
+
   updateInventory(ingr: Drop){
-    this.myInventory[this.myInventory.findIndex(({ name }) => name === ingr.name)].value += ingr.value;
+    this.myInventory[this.myInventory.findIndex(({ name }) => name === ingr.name)].quantity += ingr.quantity;
   }
-  removeInventory(){
 
+  removeInventory(ingrName: string){
+    this.myInventory.splice(this.myInventory.findIndex(({ name }) => name === ingrName), 1);
   }
-  ingrType(ingr: Drop) {
 
+  ingrExists(ingrName: string){
+    return (this.myInventory.findIndex(({ name }) => name === ingrName) !== -1);
+  }
+
+  ingrType(ingrName: string) {
+    return this.myInventory.find(({ name }) => name === ingrName).type;
+  }
+
+  ingrQuantity(ingrName: string){
+    return this.myInventory.find(({ name }) => name === ingrName).quantity;
   }
 }
