1b21a095e59aa8c4638eeb91aa1a395833784db7
Sandro T <ordysan@gmail.com>
2020-04-14 17:29:19 +0200

    minor fix

--- src/app/components/recipelist/recipe/recipe.component.ts
+++ src/app/components/recipelist/recipe/recipe.component.ts
@@ -55,7 +55,6 @@ export class RecipeComponent implements OnChanges{
         }
       }
       formatedIngr.name = ingr.name;
-      // console.log(formatedIngr);
       this.displayRecipe.push(formatedIngr);
     });
   }
