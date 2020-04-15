262a4c6578c10d9d0f0a78eb6f7f893502f2528e
Sandro T <ordysan@gmail.com>
2020-04-15 19:54:40 +0200

    inventory editing functions

--- src/app/components/inventory/inventory.component.ts
+++ src/app/components/inventory/inventory.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { InventoryService } from 'src/app/services/inventory.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 interface Drop { type: number; name: string; quantity: number }
 
@@ -8,15 +9,41 @@ interface Drop { type: number; name: string; quantity: number }
   templateUrl: './inventory.component.html',
   styleUrls: ['./inventory.component.css']
 })
-export class InventoryComponent implements OnInit {
-  public myvar: string;
+export class InventoryComponent {
+  @Input() dropInput: number;
+  @Input() noConfirm: boolean;
+
+  public dropName: string;
+  public editMode = true;
   public myIngredients: Drop[] = [];
 
-  constructor(private invService: InventoryService) {
+  constructor(private invService: InventoryService, private modalService: NgbModal) {
     this.myIngredients = this.invService.myInventory;
   };
 
-  ngOnInit() {
+  deleteDrop(content: any, ingr: string){
+    this.dropName = ingr;
+    if(this.noConfirm)
+      this.invService.removeDrop(ingr);
+    else{
+      this.modalService.open(content, { size: 'sm' }).result.then(
+        res=>{
+          this.invService.removeDrop(ingr);
+        },dismiss=>{}
+      );
+    }
   }
 
+  editDrop(content: any, ingr: string) {
+    this.dropName = ingr;
+    this.dropInput = this.invService.dropQuantity(ingr);
+    this.modalService.open(content, { size: 'sm' }).result.then(
+      res=>{
+        console.log('CloseButton', res);
+        if (res > 9999)
+          res = 9999;
+        this.invService.editQuantity(ingr, res);
+      },dismiss=>{}
+    );
+  }
 }
