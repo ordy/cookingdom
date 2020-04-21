21a87b9e6853cde31ffa60c3097caa5e73d77986
Sandro T <ordysan@gmail.com>
2020-04-21 21:10:15 +0200

    firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyDLpt49nq_mTuffNuSBIWIonCLG-nET4Z4',
+    authDomain: 'cookingdom-ng.firebaseapp.com',
+    databaseURL: 'https://cookingdom-ng.firebaseio.com',
+    projectId: 'cookingdom-ng',
+    storageBucket: 'cookingdom-ng.appspot.com',
+    messagingSenderId: '568889147655',
+    appId: '1:568889147655:web:90fc9da6fc86df7d1585a2',
+    measurementId: 'G-5TWBHD0QBM'
+  }
 };
 
 /*
