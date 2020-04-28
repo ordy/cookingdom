d207d8d4cec3a877eb91422f9485532d0fd33058
Sandro T <ordysan@gmail.com>
2020-04-29 00:10:59 +0200

    dynamic page title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
 import { AuthService } from './services/auth.service';
+import { Title } from '@angular/platform-browser';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 
 
 @Component({
@@ -12,15 +15,22 @@ export class AppComponent implements OnInit {
   public currentYear: number = new Date().getFullYear();
   public gitIcon = faGithubAlt;
 
-  constructor(private authS: AuthService) {}
+  constructor(private activatedRoute: ActivatedRoute,
+              private titleService: Title,
+              private router: Router) {}
 
   ngOnInit() {
+    // Setting the active route as page title
+    const appTitle = this.titleService.getTitle();
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd),
+      map(() => {
+        const child = this.activatedRoute.firstChild;
+        if (child.snapshot.data.title) {
+          return child.snapshot.data.title;
+        }
+        return appTitle;
+      })).subscribe((title: string) => {
+            this.titleService.setTitle(title);
+          });
   }
-
-  isLoggedIn(): boolean {
-    // console.log(this.authS.user);
-    return true;
-    // return this.authS.user !== null ? true : false;
-  }
-
 }
