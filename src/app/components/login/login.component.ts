e51aa808a2fdce33a416d10ddf03c1f19676ffe2
Sandro T <ordysan@gmail.com>
2020-04-28 02:57:10 +0200

    created login component, Auth service

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service'
+import { User } from 'firebase';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public user: User;
+  public saveLogin = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authS: AuthService) {
   }
 
+  ngOnInit(): void {
+    this.authS.signOut();
+  }
+
+  signIn(form: NgForm){
+    const email: string = form.value.email;
+    const password: string = form.value.password;
+    this.authS.SignIn(email, password, this.saveLogin);
+    this.authS.getValue();
+    form.reset();
+  }
+
+  signOut(){
+    this.authS.signOut();
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,80 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { User } from  'firebase';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { FirebaseApp } from '@angular/fire';
+import { Observable } from 'rxjs';
+import * as firebase from 'firebase';
+import { Router } from '@angular/router';
+import { Session } from 'inspector';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  user : User;
+  field : string;
+  userData : any;
 
-  constructor() { }
+  constructor(public fireAuth: AngularFireAuth, private route: Router) {
+    this.fireAuth.onAuthStateChanged(user => {
+      if (user) {
+        this.fireAuth.authState.subscribe(res => {
+          this.user = res;
+        });
+      } else {
+        console.log('user is NOT signed in');
+      }
+    })
+  }
+
+  isLoggedIn() {
+    this.fireAuth.authState.subscribe(res => {
+      if (res === null){
+        console.log('empty');
+      }else{
+        console.log('not empty');
+      }
+    })
+  }
+
+  currentUser(){
+    this.fireAuth.onAuthStateChanged(user => {
+      if (user) {
+        console.log('user is signed in');
+        this.user = firebase.auth().currentUser;
+       // console.log('user is',this.user);
+      } else {
+        console.log('user is NOT signed in');
+      }
+    })
+   //  this.route.navigateByUrl('/recipes');
+  }
+
+  signOut(){
+    this.fireAuth.signOut();
+    this.user = null;
+  }
+
+  async SignIn(username: string, password: string, keepLocal: boolean) {
+    // set persistence state if user wants to stay logged in
+    if (keepLocal){
+      this.fireAuth.setPersistence('local');
+    }
+    else{
+      this.fireAuth.setPersistence('session');
+    }
+    // Auth and redirection to homepage
+    return this.fireAuth.signInWithEmailAndPassword(username, password)
+      .then((result) => {
+        this.route.navigateByUrl('');
+      }).catch((error) => {
+        window.alert(error.message)
+      })
+  }
+  getValue(){
+    this.fireAuth.authState.subscribe(res => {
+      this.user = res;
+    });
+  }
 }
