e86300acd436680f50ca7b3075a422471742e02f
Sandro T <ordysan@gmail.com>
2020-04-28 02:50:43 +0200

    new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,13 @@ import { RecipeListComponent } from './components/recipelist/recipelist.componen
 import { InventoryComponent } from './components/inventory/inventory.component';
 import { IngredientsComponent } from './components/ingredients/ingredients.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/login/register/register.component';
 
 const routes: Routes = [
+  {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
   {path: 'recipes', component: RecipeListComponent},
   {path: 'inventory', component: InventoryComponent},
   {path: 'ingredients', component: IngredientsComponent}
