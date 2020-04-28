ac73cc52eee34e1d0737e72791660173ee2bc6fa
Sandro T <ordysan@gmail.com>
2020-04-28 23:43:01 +0200

    created alt header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faCarrot, faMortarPestle, faBriefcase, faUser } from '@fortawesome/free-solid-svg-icons'
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent{
+export class HeaderComponent implements OnInit{
   public title = 'CooKingdom';
   public ingrIcon = faCarrot;
   public rcpIcon = faMortarPestle;
   public invIcon = faBriefcase;
   public userIcon = faUser;
   public isMenuCollapsed = true;
+  public isLoggedIn: Observable<boolean>;
+
+  constructor(private authS: AuthService) {}
+
+  ngOnInit() {
+    this.isLoggedIn = this.authS.isLoggedIn;
+  }
 }
