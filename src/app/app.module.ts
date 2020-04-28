73c751aba28a6707ac79b736f13f16fbe61c137e
Sandro T <ordysan@gmail.com>
2020-04-28 02:53:15 +0200

    new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule  } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from 'src/environments/environment';
-
 import { AppComponent } from './app.component';
 import { InventoryComponent } from './components/inventory/inventory.component';
 import { RecipeListComponent } from './components/recipelist/recipelist.component';
@@ -14,6 +14,10 @@ import { IngredientsComponent } from './components/ingredients/ingredients.compo
 import { InventoryService } from './services/inventory.service';
 import { RecipeComponent } from './components/recipelist/recipe/recipe.component';
 import { LoginComponent } from './components/login/login.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/login/register/register.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +26,10 @@ import { LoginComponent } from './components/login/login.component';
     RecipeListComponent,
     IngredientsComponent,
     RecipeComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +37,9 @@ import { LoginComponent } from './components/login/login.component';
     NgbModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    FontAwesomeModule
   ],
   providers: [ InventoryService ],
   bootstrap: [ AppComponent ]
