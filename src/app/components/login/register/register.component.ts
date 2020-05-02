beca916def9ba96dae808862e2d2bf62a5885642
Sandro T <ordysan@gmail.com>
2020-05-02 04:37:08 +0200

    registration formGroup, field validation

--- src/app/components/login/register/register.component.ts
+++ src/app/components/login/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { faGoogle, faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { ReactiveFormsModule, FormsModule, FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  public displayForm = false;
+  public googleIcon = faGoogle;
+  public fbIcon = faFacebook;
+  public mailIcon = faEnvelope;
+  signupForm: FormGroup;
+  username = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  // pass: at least one lower, one upper, one special, min-lenght 6
+  password = new FormControl('',
+    [Validators.required,
+    Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_])[A-Za-z0-9!@#$%^&*-_]{6,}')]);
+  pwconfirmation = new FormControl('',
+    [Validators.required,
+    Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_])[A-Za-z0-9!@#$%^&*-_]{6,}')]);
 
-  constructor() { }
+  constructor(private authS: AuthService, public fb: FormBuilder) {
+    this.signupForm = fb.group({
+      username: this.username,
+      email: this.email,
+      password: this.password,
+      passconfirm: this.pwconfirmation
+    }, { validator: this.passcheck });
+  }
 
   ngOnInit(): void {
+  }
+  googleRegister() {
+    this.authS.googleSignUp();
+  }
+  facebookRegister() {
+    this.authS.facebookSignUp();
+  }
+  signUp() {
+
+  }
+
+  passcheck(form: FormGroup) {
+    const pass = form.get('password').value;
+    const passconf = form.get('passconfirm').value;
+    if (pass === passconf) {
+      return null;
+    } else {
+      form.controls.passconfirm.setErrors({ incorrect: true });
+      return { notSame: true };
+    }
   }
 
 }
--- src/app/components/recipelist/recipe/recipe.component.ts
+++ src/app/components/recipelist/recipe/recipe.component.ts
@@ -1,32 +1,31 @@
 import { Component, Input, OnChanges } from '@angular/core';
-import { match } from 'assert';
 
-interface Ingredient {type: number, name: string, quantity: number};
-interface DisplayIngr {type: string, name: string, quantity: string};
-interface Recipe {name: string, duration: number, difficulty: string, ingredientsList: Ingredient[], instructions: string[]};
+interface Ingredient { type: number, name: string, quantity: number };
+interface DisplayIngr { type: string, name: string, quantity: string };
+interface Recipe { name: string, duration: number, difficulty: string, ingredientsList: Ingredient[], instructions: string[] };
 
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
   styleUrls: ['./recipe.component.css']
 })
-export class RecipeComponent implements OnChanges{
+export class RecipeComponent implements OnChanges {
   public displayRecipe: DisplayIngr[] = [];
-  public fractionsTable: string[] = ['1/4','1/2','3/4'];
+  public fractionsTable: string[] = ['1/4', '1/2', '3/4'];
 
   @Input() rcpName: Recipe;
 
-  ngOnChanges(){
+  ngOnChanges() {
     let formatedIngr: DisplayIngr;
     this.rcpName.ingredientsList.forEach(ingr => {
-      formatedIngr = {type: '', name: '', quantity: ''};
-      switch(ingr.type){
+      formatedIngr = { type: '', name: '', quantity: '' };
+      switch (ingr.type) {
         case 1: {
           formatedIngr.type = '';
           const fraction = ingr.quantity % 1;
-          if(fraction !== 0){ // check if we have fraction of ingredient
-            formatedIngr.quantity = this.fractionsTable[fraction/0.25 - 1];
-            if(Math.trunc(ingr.quantity) > 0)
+          if (fraction !== 0) { // check if we have fraction of ingredient
+            formatedIngr.quantity = this.fractionsTable[fraction / 0.25 - 1];
+            if (Math.trunc(ingr.quantity) > 0)
               formatedIngr.quantity = Math.trunc(ingr.quantity).toString() + '+' + formatedIngr.quantity;
           }
           else
@@ -34,20 +33,20 @@ export class RecipeComponent implements OnChanges{
           break;
         }
         case 2: {
-          if(Math.trunc(ingr.quantity) === 0){
+          if (Math.trunc(ingr.quantity) === 0) {
             formatedIngr.type = 'g';
-            formatedIngr.quantity = (ingr.quantity%1*1000).toString();
-          }else{
+            formatedIngr.quantity = (ingr.quantity % 1 * 1000).toString();
+          } else {
             formatedIngr.type = 'kg';
             formatedIngr.quantity = ingr.quantity.toString();
           }
           break;
         }
         case 3: {
-          if(Math.trunc(ingr.quantity) === 0){
+          if (Math.trunc(ingr.quantity) === 0) {
             formatedIngr.type = 'ml';
-            formatedIngr.quantity = (ingr.quantity%1*1000).toString();
-          }else{
+            formatedIngr.quantity = (ingr.quantity % 1 * 1000).toString();
+          } else {
             formatedIngr.type = 'L';
             formatedIngr.quantity = ingr.quantity.toString();
           }
@@ -59,7 +58,7 @@ export class RecipeComponent implements OnChanges{
     });
   }
 
-  getImagePath(){
+  getImagePath() {
     return '/assets/recipes/' + encodeURI(this.rcpName.name.replace(/\s+/g, '-').toLowerCase()).replace(/\'/g, '') + '.jpg';
   }
 }
