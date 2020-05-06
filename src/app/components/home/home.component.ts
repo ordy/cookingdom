5814f21c686ed0ee6d73aa4abe4a5777366854bf
Sandro T <ordysan@gmail.com>
2020-05-06 16:47:06 +0200

    created homepage

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public status = '';
+  constructor(private authS: AuthService) {
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
 
+  checkVerified(){
+    if(this.authS.checkVerified())
+      this.status = 'Acccount ' + this.authS.user.email + ' is VERIFIED!';
+    else
+      this.status = 'Acccount ' + this.authS.user.email + ' is NOT VERIFIED!';
+  }
 }
