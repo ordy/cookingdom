6ef2da28fdb355c49ac2881fc1c85167548126fe
Sandro T <ordysan@gmail.com>
2020-05-15 21:21:50 +0200

    activating routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,20 +7,22 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/login/register/register.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
+import { RouteGuard } from './guards/route.guard';
+import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent, data: {title: 'CooKingdom'}},
-  {path: 'login', component: LoginComponent, data: {title: 'Login - CD'}},
-  {path: 'ingredients', component: IngredientsComponent, data: {title: 'Ingredients - CD'}},
-  {path: 'inventory', component: InventoryComponent, data: {title: 'Inventory - CD'}},
-  {path: 'recipes', component: RecipeListComponent, data: {title: 'Recipes - CD'}},
-  {path: 'register', component: RegisterComponent, data: {title: 'Register - CD'}},
-  {path: '**', component: NotfoundComponent, data: {title: 'Not-Found'}}
+  { path: '', component: HomeComponent, canActivate: [RouteGuard], data: { title: 'CooKingdom' } },
+  { path: 'login', component: LoginComponent, canActivate: [LoginGuard], data: { title: 'Login - CD' } },
+  { path: 'ingredients', component: IngredientsComponent, canActivate: [RouteGuard], data: { title: 'Ingredients - CD' } },
+  { path: 'inventory', component: InventoryComponent, canActivate: [RouteGuard], data: { title: 'Inventory - CD' } },
+  { path: 'recipes', component: RecipeListComponent, canActivate: [RouteGuard], data: { title: 'Recipes - CD' } },
+  { path: 'register', component: RegisterComponent, canActivate: [LoginGuard], data: { title: 'Register - CD' } },
+  { path: '**', component: NotfoundComponent, data: { title: 'Not-Found' } }
 ];
 
 @NgModule({
   declarations: [],
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
