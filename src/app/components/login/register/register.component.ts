91b206b93851360a4de86d98ac50da577588a36c
Sandro T <ordysan@gmail.com>
2020-05-27 00:12:29 +0200

    Field validation and tooltip

--- src/app/components/login/register/register.component.ts
+++ src/app/components/login/register/register.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { faGoogle, faFacebook } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, Validators, ValidationErrors } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { debounceTime, switchMap, map, take, mergeMap, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-register',
@@ -10,25 +12,28 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  public displayForm = false;
   public googleIcon = faGoogle;
   public fbIcon = faFacebook;
   public mailIcon = faEnvelope;
-  private pwPattern = '(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_])[A-Za-z0-9!@#$%^&*-_]{6,}';
-  private usrPattern = '[A-Za-z0-9]{3,15}';
+  // regex: at least one lower, one upper, one special, 6 chars min-length
+  private pwPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_])[A-Za-z0-9!@#$%^&*-_]{6,}$/;
+  private usrPattern = /^[A-Za-z0-9]{3,20}$/;
+  private lastUsername: string;
+  private userExists: ValidationErrors;
   public providerUsername: string;
+
+  public debouncer: any;
+
   public signupForm: FormGroup;
   public usernameForm: FormGroup;
-  public username = new FormControl('',
-    [Validators.required,
-    Validators.pattern(this.usrPattern)]);
+  public username = new FormControl('', [Validators.required, Validators.pattern(this.usrPattern)], this.usernameCheck.bind(this));
   private email = new FormControl('', [Validators.required, Validators.email]);
-  // pass: at least one lower, one upper, one special, min-lenght 6
   private password = new FormControl('', [Validators.required, Validators.pattern(this.pwPattern)]);
   private pwConfirm = new FormControl('', [Validators.required, Validators.pattern(this.pwPattern)]);
 
-  constructor(private authS: AuthService, private fb: FormBuilder) {
+  constructor(public authS: AuthService, private fb: FormBuilder) {
     this.signupForm = fb.group({
+
       username: this.username,
       email: this.email,
       password: this.password,
@@ -37,6 +42,7 @@ export class RegisterComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
   }
 
   signUp() {
@@ -57,7 +63,35 @@ export class RegisterComponent implements OnInit {
     }
   }
 
-  saveUsername() {
-    // to-do
+  usernameCheck(username: FormControl): Promise<any> | Observable<any> {
+    // reseting debouce time on every validator call
+    clearTimeout(this.debouncer);
+    const promise = new Promise<any>((resolve, reject) => {
+      this.debouncer = setTimeout(() => {
+        if (username.value !== this.lastUsername) {
+          this.lastUsername = username.value;
+          const isTaken = this.authS.usernameTaken(username.value.toLowerCase());
+          this.userExists = isTaken.then(res => {
+            if (!res)
+              return { userExists: true };
+            else
+              return null;
+          });
+        }
+        resolve(this.userExists);
+      }, 1700)
+    });
+    return promise;
+  }
+
+  displayTooltip(pattern: string) {
+    let regX: RegExp;
+    if (pattern === 'user') {
+      regX = new RegExp(this.usrPattern);
+      return regX.test(this.username.value) ? false : true;
+    } else {
+      regX = new RegExp(this.pwPattern);
+      return regX.test(this.password.value) ? false : true;
+    }
   }
 }
