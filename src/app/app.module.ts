8ee0a4e85c0fd98073ed7375112be3e6f8ac441b
Sandro T <ordysan@gmail.com>
2020-05-28 21:52:12 +0200

    syntaxe fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { environment } from 'src/environments/environment';
+
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { environment } from 'src/environments/environment';
+
 import { AppComponent } from './app.component';
 import { InventoryComponent } from './components/inventory/inventory.component';
 import { RecipeListComponent } from './components/recipelist/recipelist.component';
@@ -14,7 +17,6 @@ import { IngredientsComponent } from './components/ingredients/ingredients.compo
 import { InventoryService } from './services/inventory.service';
 import { RecipeComponent } from './components/recipelist/recipe/recipe.component';
 import { LoginComponent } from './components/login/login.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/login/register/register.component';
--- src/app/components/recipelist/recipelist.component.ts
+++ src/app/components/recipelist/recipelist.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { InventoryService } from 'src/app/services/inventory.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-interface Ingredient {type: number, name: string, quantity: number};
-interface Recipe {name: string, duration: number, difficulty: string, ingredientsList: Ingredient[], instructions: string[]};
+interface Ingredient { type: number, name: string, quantity: number };
+interface Recipe { name: string, duration: number, difficulty: string, ingredientsList: Ingredient[], instructions: string[] };
 
 const recipesDB: Recipe[] = [
-  {name: 'Chilli Con Carne', difficulty: 'A', duration: 10,
+  {
+    name: 'Chilli Con Carne', difficulty: 'A', duration: 10,
     ingredientsList: [
-      { type: 2, name: 'Salt', quantity: 1},
-      { type: 1, name: 'Tomato', quantity: 2},
-      { type: 2, name: 'Chicken', quantity: 4},
-      { type: 2, name: 'Spaghetti', quantity: 8},
-      { type: 1, name: 'Apple', quantity: 16}
+      { type: 2, name: 'Salt', quantity: 1 },
+      { type: 1, name: 'Tomato', quantity: 2 },
+      { type: 2, name: 'Chicken', quantity: 4 },
+      { type: 2, name: 'Spaghetti', quantity: 8 },
+      { type: 1, name: 'Apple', quantity: 16 }
     ],
     instructions: [
       'Nullam nec porta ligula, id pellentesque dui. Quisque semper eget arcu sit amet fermentum.',
@@ -20,24 +22,28 @@ const recipesDB: Recipe[] = [
       'Cras est enim, facilisis in ante in, ullamcorper sollicitudin est. Nulla fermentum, arcu ac lacinia scelerisque.',
       'Sed eget lacinia nunc. Curabitur ac semper justo, a sollicitudin turpis. Suspendisse semper.',
       'Aenean et scelerisque nisl, at placerat dolor. Curabitur sit amet molestie lectus, vitae hendrerit augue.'
-    ]},
-  {name: 'Chocolate Cookies', difficulty: 'B', duration: 15,
+    ]
+  },
+  {
+    name: 'Chocolate Cookies', difficulty: 'B', duration: 15,
     ingredientsList: [
-      { type: 2, name: 'Pork', quantity: 1},
-      { type: 2, name: 'Pepper', quantity: 2},
-      { type: 2, name: 'Potato', quantity: 3},
-      { type: 2, name: 'Curry', quantity: 4}
+      { type: 2, name: 'Pork', quantity: 1 },
+      { type: 2, name: 'Pepper', quantity: 2 },
+      { type: 2, name: 'Potato', quantity: 3 },
+      { type: 2, name: 'Curry', quantity: 4 }
     ],
     instructions: [
       'Nullam nec porta ligula, id pellentesque dui. Quisque semper eget arcu sit amet fermentum.',
       'Donec aliquet tortor a iaculis tempus. Vestibulum vel nunc urna. Sed ullamcorper lacus vitae bibendum porttitor.',
       'Duis erat arcu, sollicitudin in arcu et, euismod molestie est. Fusce auctor lorem id consectetur fermentum.'
-    ]},
-  {name: 'Grilled Cheese', difficulty: 'C', duration: 20,
+    ]
+  },
+  {
+    name: 'Grilled Cheese', difficulty: 'C', duration: 20,
     ingredientsList: [
-      { type: 2, name: 'Sardines', quantity: 10},
-      { type: 2, name: 'Spaghetti', quantity: 20},
-      { type: 1, name: 'Tomato', quantity: 30}
+      { type: 2, name: 'Sardines', quantity: 10 },
+      { type: 2, name: 'Spaghetti', quantity: 20 },
+      { type: 1, name: 'Tomato', quantity: 30 }
     ],
     instructions: [
       'Nullam nec porta ligula, id pellentesque dui. Quisque semper eget arcu sit amet fermentum.',
@@ -46,45 +52,53 @@ const recipesDB: Recipe[] = [
       'Cras est enim, facilisis in ante in, ullamcorper sollicitudin est. Nulla fermentum, arcu ac lacinia scelerisque.',
       'Sed eget lacinia nunc. Curabitur ac semper justo, a sollicitudin turpis. Suspendisse semper.',
       'Aenean et scelerisque nisl, at placerat dolor. Curabitur sit amet molestie lectus, vitae hendrerit augue.'
-    ]},
-  {name: 'Macaroni and Cheese', difficulty: 'D', duration: 40,
+    ]
+  },
+  {
+    name: 'Macaroni and Cheese', difficulty: 'D', duration: 40,
     ingredientsList: [
-      { type: 1, name: 'Banana', quantity: 11},
-      { type: 1, name: 'Apple', quantity: 22},
-      { type: 1, name: 'Orange', quantity: 33},
-      { type: 3, name: 'Water', quantity: 44}
+      { type: 1, name: 'Banana', quantity: 11 },
+      { type: 1, name: 'Apple', quantity: 22 },
+      { type: 1, name: 'Orange', quantity: 33 },
+      { type: 3, name: 'Water', quantity: 44 }
     ],
     instructions: [
       'Nullam nec porta ligula, id pellentesque dui. Quisque semper eget arcu sit amet fermentum.',
       'Donec aliquet tortor a iaculis tempus. Vestibulum vel nunc urna. Sed ullamcorper lacus vitae bibendum porttitor.',
       'Duis erat arcu, sollicitudin in arcu et, euismod molestie est. Fusce auctor lorem id consectetur fermentum.',
       'Cras est enim, facilisis in ante in, ullamcorper sollicitudin est. Nulla fermentum, arcu ac lacinia scelerisque.'
-    ]},
-  {name: 'Paella', difficulty: 'S', duration: 120,
+    ]
+  },
+  {
+    name: 'Paella', difficulty: 'S', duration: 120,
     ingredientsList: [
-      { type: 1, name: 'Crab', quantity: 1},
-      { type: 1, name: 'Tomato', quantity: 2},
-      { type: 2, name: 'Sardines', quantity: 3}
+      { type: 1, name: 'Crab', quantity: 1 },
+      { type: 1, name: 'Tomato', quantity: 2 },
+      { type: 2, name: 'Sardines', quantity: 3 }
     ],
     instructions: [
       'Nullam nec porta ligula, id pellentesque dui. Quisque semper eget arcu sit amet fermentum.'
-    ]},
-  {name: 'Caesar Salad', difficulty: 'A', duration: 30,
+    ]
+  },
+  {
+    name: 'Caesar Salad', difficulty: 'A', duration: 30,
     ingredientsList: [
-      { type: 3, name: 'Olive oil', quantity: 1},
-      { type: 1, name: 'Tomato', quantity: 3},
-      { type: 2, name: 'Shrimps', quantity: 4},
-      { type: 2, name: 'Pepper', quantity: 8}
+      { type: 3, name: 'Olive oil', quantity: 1 },
+      { type: 1, name: 'Tomato', quantity: 3 },
+      { type: 2, name: 'Shrimps', quantity: 4 },
+      { type: 2, name: 'Pepper', quantity: 8 }
     ],
     instructions: [
       'Nullam nec porta ligula, id pellentesque dui. Quisque semper eget arcu sit amet fermentum.',
       'Donec aliquet tortor a iaculis tempus. Vestibulum vel nunc urna. Sed ullamcorper lacus vitae bibendum porttitor.'
-    ]},
-  {name: 'Apple Pie', difficulty: 'B', duration: 10,
+    ]
+  },
+  {
+    name: 'Apple Pie', difficulty: 'B', duration: 10,
     ingredientsList: [
-      { type: 2, name: 'Curry', quantity: 4},
-      { type: 2, name: 'Macaroni', quantity: 23},
-      { type: 2, name: 'Chicken', quantity: 3}
+      { type: 2, name: 'Curry', quantity: 4 },
+      { type: 2, name: 'Macaroni', quantity: 23 },
+      { type: 2, name: 'Chicken', quantity: 3 }
     ],
     instructions: [
       'Nullam nec porta ligula, id pellentesque dui. Quisque semper eget arcu sit amet fermentum.',
@@ -93,13 +107,15 @@ const recipesDB: Recipe[] = [
       'Cras est enim, facilisis in ante in, ullamcorper sollicitudin est. Nulla fermentum, arcu ac lacinia scelerisque.',
       'Sed eget lacinia nunc. Curabitur ac semper justo, a sollicitudin turpis. Suspendisse semper.',
       'Aenean et scelerisque nisl, at placerat dolor. Curabitur sit amet molestie lectus, vitae hendrerit augue.'
-    ]},
-  {name: 'Scrambled Eggs', difficulty: 'C', duration: 5,
+    ]
+  },
+  {
+    name: 'Scrambled Eggs', difficulty: 'C', duration: 5,
     ingredientsList: [
-      { type: 1, name: 'Lobster', quantity: 1},
-      { type: 2, name: 'Macaroni', quantity: 6},
-      { type: 2, name: 'Potato', quantity: 7},
-      { type: 2, name: 'Salt', quantity: 8}
+      { type: 1, name: 'Lobster', quantity: 1 },
+      { type: 2, name: 'Macaroni', quantity: 6 },
+      { type: 2, name: 'Potato', quantity: 7 },
+      { type: 2, name: 'Salt', quantity: 8 }
     ],
     instructions: [
       'Nullam nec porta ligula, id pellentesque dui. Quisque semper eget arcu sit amet fermentum.',
@@ -107,32 +123,37 @@ const recipesDB: Recipe[] = [
       'Duis erat arcu, sollicitudin in arcu et, euismod molestie est. Fusce auctor lorem id consectetur fermentum.',
       'Cras est enim, facilisis in ante in, ullamcorper sollicitudin est. Nulla fermentum, arcu ac lacinia scelerisque.',
       'Sed eget lacinia nunc. Curabitur ac semper justo, a sollicitudin turpis. Suspendisse semper.'
-    ]},
-  {name: 'Lasagna', difficulty: 'D', duration: 15,
+    ]
+  },
+  {
+    name: 'Lasagna', difficulty: 'D', duration: 15,
     ingredientsList: [
-      { type: 2, name: 'Pork', quantity: 1},
-      { type: 2, name: 'Lamb', quantity: 5},
-      { type: 2, name: 'Pepper', quantity: 8},
-      { type: 2, name: 'Chicken', quantity: 12}
+      { type: 2, name: 'Pork', quantity: 1 },
+      { type: 2, name: 'Lamb', quantity: 5 },
+      { type: 2, name: 'Pepper', quantity: 8 },
+      { type: 2, name: 'Chicken', quantity: 12 }
     ],
     instructions: [
       'Nullam nec porta ligula, id pellentesque dui. Quisque semper eget arcu sit amet fermentum.',
       'Donec aliquet tortor a iaculis tempus. Vestibulum vel nunc urna. Sed ullamcorper lacus vitae bibendum porttitor.'
-    ]},
-  {name: 'Teriyaki Chicken', difficulty: 'S', duration: 180,
+    ]
+  },
+  {
+    name: 'Teriyaki Chicken', difficulty: 'S', duration: 180,
     ingredientsList: [
-      { type: 2, name: 'Sardines', quantity: 0.4},
-      { type: 1, name: 'Banana', quantity: 12},
-      { type: 1, name: 'Crab', quantity: 5.75},
-      { type: 2, name: 'Salt', quantity: 21},
-      { type: 3, name: 'Pepper', quantity: 0.3}
+      { type: 2, name: 'Sardines', quantity: 0.4 },
+      { type: 1, name: 'Banana', quantity: 12 },
+      { type: 1, name: 'Crab', quantity: 5.75 },
+      { type: 2, name: 'Salt', quantity: 21 },
+      { type: 3, name: 'Pepper', quantity: 0.3 }
     ],
     instructions: [
       'Nullam nec porta ligula, id pellentesque dui. Quisque semper eget arcu sit amet fermentum.',
       'Donec aliquet tortor a iaculis tempus. Vestibulum vel nunc urna. Sed ullamcorper lacus vitae bibendum porttitor.',
       'Duis erat arcu, sollicitudin in arcu et, euismod molestie est. Fusce auctor lorem id consectetur fermentum.',
       'Cras est enim, facilisis in ante in, ullamcorper sollicitudin est. Nulla fermentum, arcu ac lacinia scelerisque.'
-    ]}
+    ]
+  }
 ];
 
 @Component({
@@ -152,10 +173,10 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
-  searchRecipes(){
+  searchRecipes() {
     this.recipeList = [];
-    recipesDB.forEach( (recipe) => {
-       const avaibleRcp = recipe.ingredientsList.filter(ingr => this.ingrCheck(ingr.name, ingr.quantity));
+    recipesDB.forEach((recipe) => {
+      const avaibleRcp = recipe.ingredientsList.filter(ingr => this.ingrCheck(ingr.name, ingr.quantity));
       // make recipe avaible if enough ingredients
       if (avaibleRcp.length >= (recipe.ingredientsList.length - this.offsetValue))
         this.recipeList.push(recipe.name);
@@ -165,16 +186,16 @@ export class RecipeListComponent implements OnInit {
     this.showListToggle = false;
   }
 
-  ingrCheck(name: string, quantity: number){
+  ingrCheck(name: string, quantity: number) {
     return this.invService.dropExists(name) && this.invService.dropQuantity(name) >= quantity;
   }
 
-  toggleList(){
+  toggleList() {
     this.showListToggle = !this.showListToggle;
     this.displayRec = !this.displayRec;
   }
 
-  showRecipe(listElem: string){
+  showRecipe(listElem: string) {
     this.toggleList();
     this.recipeParent = recipesDB.find(recipe => recipe.name === listElem);
   }
