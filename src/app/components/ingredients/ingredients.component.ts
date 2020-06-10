d338a736d1ee3fb5d41c6ee09ef1ec148ddfb769
Sandro T <ordysan@gmail.com>
2020-06-10 22:32:33 +0200

    firestore ingredients typeahead

--- src/app/components/ingredients/ingredients.component.ts
+++ src/app/components/ingredients/ingredients.component.ts
@@ -1,86 +1,75 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { InventoryService } from '../../services/inventory.service';
 import { AngularFirestore } from '@angular/fire/firestore';
 
 // type: 1=unit 2=mass 3=volume
-interface Ingredient { type: number; name: string }
-interface Drop { type: number; name: string; quantity: number }
-
-// type: 1=unit 2=mass 3=volume
-const ingredients: Ingredient[] = [
-  { type: 2, name: 'Salt' },
-  { type: 2, name: 'Pepper' },
-  { type: 1, name: 'Tomato' },
-  { type: 1, name: 'Cucumber' },
-  { type: 1, name: 'Apple' },
-  { type: 1, name: 'Orange' },
-  { type: 1, name: 'Banana' },
-  { type: 2, name: 'Chicken' },
-  { type: 2, name: 'Lamb' },
-  { type: 2, name: 'Pork' },
-  { type: 1, name: 'Lobster' },
-  { type: 1, name: 'Crab' },
-  { type: 2, name: 'Shrimps' },
-  { type: 1, name: 'Salmon' },
-  { type: 2, name: 'Spaghetti' },
-  { type: 2, name: 'Fusilli' },
-  { type: 2, name: 'Sardines' },
-  { type: 2, name: 'Macaroni' },
-  { type: 2, name: 'Potato' },
-  { type: 2, name: 'Curry' },
-  { type: 3, name: 'Water' },
-  { type: 3, name: 'Olive Oil' }
-];
+interface Ingredient {
+	type: number;
+	name: string;
+}
+interface Drop {
+	type: number;
+	name: string;
+	quantity: number;
+}
 
 @Component({
-  selector: 'app-ingredients',
-  templateUrl: './ingredients.component.html',
-  styleUrls: ['./ingredients.component.css']
+	selector: 'app-ingredients',
+	templateUrl: './ingredients.component.html',
+	styleUrls: ['./ingredients.component.css'],
 })
 export class IngredientsComponent implements OnInit {
-  public selectedIngr: Ingredient;
-  public selectedIngrValue = 1;
-  public newIngredients: Drop[] = [];
+	public selectedIngr: Ingredient;
+	public selectedIngrValue = 1;
+	public newIngredients: Drop[] = [];
 
-  formatter = (ingredient: Ingredient) => ingredient.name;
+	constructor(private invS: InventoryService, public db: AngularFirestore) {}
 
-  search = (text: Observable<string>) =>
-    text.pipe(
-      debounceTime(200),
-      distinctUntilChanged(),
-      map(term => term === '' ? [] : ingredients.filter(v => v.name.toLowerCase().indexOf(term.toLowerCase()) === 0).slice(0, 10))
-    )
+	ngOnInit() {}
 
-  constructor(private invService: InventoryService, public db: AngularFirestore) { };
+	listNotEmpty(): boolean {
+		return this.newIngredients.length > 0;
+	}
 
-  ngOnInit() { }
+	ingredientType(): number {
+		return this.selectedIngr?.type;
+	}
 
-  listNotEmpty(): boolean {
-    return this.newIngredients.length > 0;
-  }
+	newIngr() {
+		const food = {
+			type: this.selectedIngr.type,
+			name: this.selectedIngr.name,
+			quantity: Math.abs(this.selectedIngrValue),
+		};
+		const result = this.newIngredients.findIndex(({ name }) => name === food.name);
+		if (this.newIngredients.length !== 0 && result !== -1) {
+			this.newIngredients[result].quantity += food.quantity;
+		} else {
+			this.newIngredients.push(food);
+		}
+	}
 
-  ingredientType(): number {
-    return this.selectedIngr?.type;
-  }
+	addIngredients() {
+		this.invS.addInventory(this.newIngredients);
+		this.newIngredients = [];
+	}
 
-  newIngr() {
-    const food = {
-      type: this.selectedIngr.type,
-      name: this.selectedIngr.name,
-      quantity: Math.abs(this.selectedIngrValue)
-    };
-    const result = this.newIngredients.findIndex(({ name }) => name === food.name);
-    if ((this.newIngredients.length !== 0) && result !== -1) {
-      this.newIngredients[result].quantity += food.quantity;
-    } else {
-      this.newIngredients.push(food);
-    }
-  }
+	formatter = (ingredient: Ingredient) => ingredient.name;
 
-  addIngredients() {
-    this.invService.addInventory(this.newIngredients);
-    this.newIngredients = [];
-  }
+	search = (text: Observable<string>) =>
+		text.pipe(
+			debounceTime(500),
+			distinctUntilChanged(),
+			switchMap(search => {
+				if (search.length > 1) {
+					search = search.toLowerCase();
+					const offset = search + '\uf8ff'; // High value UTF character
+					return this.db
+						.collection('ingredients', ref => ref.orderBy('searchname').startAt(search).endAt(offset).limit(5))
+						.valueChanges();
+				} else return [];
+			})
+		);
 }
