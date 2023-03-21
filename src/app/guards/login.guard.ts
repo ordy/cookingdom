d7e16f566388ef5c76d75bb74e8f9d88bab86d77
Sandro T <ordysan@gmail.com>
2023-03-21 10:34:57 +0100

    updated demo route guards

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
-import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
-import { take, map, tap } from 'rxjs/operators';
 
 @Injectable({
 	providedIn: 'root',
@@ -10,21 +8,7 @@ import { take, map, tap } from 'rxjs/operators';
 export class LoginGuard implements CanActivate {
 	constructor(private authS: AuthService, private router: Router) {}
 
-	canActivate(): Observable<boolean> {
-		return this.authS.$usr.pipe(
-			take(1),
-			// false if user exists, true is null or undefined
-			map(user => !user),
-			tap(loggedIn => {
-				if (!loggedIn) {
-					console.log('Login Guard access denied.');
-					this.authS.loading.next(false);
-					this.router.navigateByUrl('/');
-				} else {
-					this.authS.loading.next(false);
-					return true;
-				}
-			})
-		);
+	canActivate() {
+		return this.authS.loggedIn.value ? false : true;
 	}
 }
--- src/app/guards/route.guard.ts
+++ src/app/guards/route.guard.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import { take, map, tap } from 'rxjs/operators';
 
 @Injectable({
 	providedIn: 'root',
@@ -9,39 +8,12 @@ import { take, map, tap } from 'rxjs/operators';
 export class RouteGuard implements CanActivate {
 	constructor(private authS: AuthService, private router: Router) {}
 
-	canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-		let uid: string;
-		this.authS.loading.next(true);
-		return this.authS.$usr.pipe(
-			take(1),
-			map(user => {
-				if (user) uid = user.uid;
-				return !!user; // true if user exists, false is null or undefined
-			}),
-			tap(loggedIn => {
-				if (!loggedIn) {
-					this.authS.loading.next(false);
-					this.router.navigateByUrl('/login');
-				} else if (this.authS.hasUsername === 'unchecked') {
-					this.authS.usernameRegistered(uid).then(user => {
-						this.authS.loading.next(false);
-						if (user === 'registered') {
-							if (state.url === '/username') {
-								this.router.navigateByUrl('/');
-							} else return true;
-						} else {
-							if (state.url !== '/username') this.router.navigateByUrl('/username');
-							else return false;
-						}
-					});
-				} else if (this.authS.hasUsername === 'unregistered') {
-					this.authS.loading.next(false);
-					if (state.url !== '/username') this.router.navigateByUrl('/username');
-					else return false;
-				} else {
-					this.authS.loading.next(false);
-				}
-			})
-		);
+	canActivate() {
+		if (this.authS.loggedIn.value) {
+			return true;
+		} else {
+			this.router.navigateByUrl('/login');
+			return false;
+		}
 	}
 }
